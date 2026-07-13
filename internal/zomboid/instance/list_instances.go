407de0ce75b6095d26ecfea3a538bfe50e67715f
Pawel <inne.poczta@gmail.com>
2026-07-13 20:50:42 +0200

    Json Schema description

--- internal/zomboid/instance/list_instances.go
+++ internal/zomboid/instance/list_instances.go
@@ -8,8 +8,8 @@ import (
 )
 
 type Instance struct {
-	Name    string
-	HomeDir string
+	Name    string `json:"name" jsonschema:"Name of the Project Zomboid server instance"`
+	HomeDir string `json:"homeDir" jsonschema:"Absolute path to the instance's home directory on disk"`
 }
 
 func (m *ZomboidInstanceManager) ListInstances(ctx context.Context, principal authorization.Principal) ([]*Instance, error) {
